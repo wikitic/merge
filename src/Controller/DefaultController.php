6bb863b4065661edd3808d32709b462efe4a1956
Miguel Ángel <migueabellan@gmail.com>
2018-11-05 09:54:00 +0100

    Add default controller

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(Request $request) : Response
+    {
+        return $this->render('base.html.twig');
+    }
+}
