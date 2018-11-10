264f4b30a72dc0ab0e6da0e4d3998a5bb809c80c
Miguel Ángel <migueabellan@gmail.com>
2018-11-10 17:35:11 +0100

    Add category entity

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        foreach ($this->getData() as $k=>$v) {
+            $category = new Category();
+            $category->setTitle($v['title']);
+            $category->setAlias($v['alias']);
+            $category->setDescription($v['description']);
+            $category->setMetatitle($v['metatitle']);
+            $category->setMetadesc($v['metadesc']);
+            $category->setMetakey($v['metakey']);
+            $category->setMetaimage($v['metaimage']);
+            $category->setActive($v['active']);
+            $category->setOrdering($v['ordering']);
+            $category->setCdate($v['cdate']);
+            $category->setMdate($v['mdate']);
+
+            $this->addReference($v['alias'], $category);
+
+            $manager->persist($category);
+        }
+    
+        $manager->flush();
+    }
+    private function getData()
+    {
+        yield [  
+            'title' => 'Categoría 1',
+            'alias' => 'categoria-1',
+            'description' => 'Descripción 1',
+            'metatitle' => 'Meta título de la categoría 1',
+            'metadesc' => 'Meta descripción de la categoría 1',
+            'metakey' => 'meta, palabras, categoría, 1',
+            'metaimage' => 'categoria.png',
+            'active' => '1',
+            'ordering' => '1',
+            'cdate' => new \DateTime('2018-11-01'),
+            'mdate' => new \DateTime('2018-11-01')
+        ];
+        yield [  
+            'title' => 'Categoría 2',
+            'alias' => 'categoria-2',
+            'description' => 'Descripción 2',
+            'metatitle' => 'Meta título de la categoría 2',
+            'metadesc' => 'Meta descripción de la categoría 2',
+            'metakey' => 'meta, palabras, categoría, 2',
+            'metaimage' => 'categoria.png',
+            'active' => '1',
+            'ordering' => '2',
+            'cdate' => new \DateTime('2018-11-01'),
+            'mdate' => new \DateTime('2018-11-01')
+        ];
+        yield [  
+            'title' => 'Categoría 3',
+            'alias' => 'categoria-3',
+            'description' => 'Descripción 3',
+            'metatitle' => 'Meta título de la categoría 3',
+            'metadesc' => 'Meta descripción de la categoría 3',
+            'metakey' => 'meta, palabras, categoría, 3',
+            'metaimage' => 'categoria.png',
+            'active' => '1',
+            'ordering' => '3',
+            'cdate' => new \DateTime('2018-11-01'),
+            'mdate' => new \DateTime('2018-11-01')
+        ];
+        yield [  
+            'title' => 'Categoría 4',
+            'alias' => 'categoria-4',
+            'description' => 'Descripción 4',
+            'metatitle' => 'Meta título de la categoría 4',
+            'metadesc' => 'Meta descripción de la categoría 4',
+            'metakey' => 'meta, palabras, categoría, 4',
+            'metaimage' => 'categoria.png',
+            'active' => '1',
+            'ordering' => '4',
+            'cdate' => new \DateTime('2018-11-01'),
+            'mdate' => new \DateTime('2018-11-01')
+        ];
+    }
+}
---
+++ src/Entity/Category.php
@@ -0,0 +1,257 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Category
+ *
+ * @ORM\Table(name="categories")
+ * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class Category
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id_category", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="alias", type="string", length=255)
+     */
+    private $alias;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="metatitle", type="string", length=255)
+     */
+    private $metatitle;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="metadesc", type="string", length=255)
+     */
+    private $metadesc;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="metakey", type="string", length=255)
+     */
+    private $metakey;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="metaimage", type="string", length=255)
+     */
+    private $metaimage;
+	
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="active", type="integer")
+     */
+    private $active = 1;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="ordering", type="integer")
+     */
+    private $ordering;
+
+	/**
+     * @var \DateTime
+     * 
+     * @ORM\Column(name="cdate", type="datetime")
+     */
+    private $cdate;
+
+    /**
+     * @var \DateTime
+     * 
+     * @ORM\Column(name="mdate", type="datetime")
+     */
+    private $mdate;
+
+
+
+    public function __construct()
+    {
+        $this->cdate    = new \DateTime();
+        $this->mdate    = new \DateTime();
+    }
+
+    /**
+     * @ORM\PreUpdate()
+     */
+    public function preUpdate()
+    {
+        $this->mdate    = new \DateTime();
+    }
+
+    
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getAlias(): ?string
+    {
+        return $this->alias;
+    }
+
+    public function setAlias(string $alias): self
+    {
+        $this->alias = $alias;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getMetatitle(): ?string
+    {
+        return $this->metatitle;
+    }
+
+    public function setMetatitle(string $metatitle): self
+    {
+        $this->metatitle = $metatitle;
+
+        return $this;
+    }
+
+    public function getMetadesc(): ?string
+    {
+        return $this->metadesc;
+    }
+
+    public function setMetadesc(string $metadesc): self
+    {
+        $this->metadesc = $metadesc;
+
+        return $this;
+    }
+
+    public function getMetakey(): ?string
+    {
+        return $this->metakey;
+    }
+
+    public function setMetakey(string $metakey): self
+    {
+        $this->metakey = $metakey;
+
+        return $this;
+    }
+
+    public function getMetaimage(): ?string
+    {
+        return $this->metaimage;
+    }
+
+    public function setMetaimage(string $metaimage): self
+    {
+        $this->metaimage = $metaimage;
+
+        return $this;
+    }
+
+    public function getActive(): ?int
+    {
+        return $this->active;
+    }
+
+    public function setActive(int $active): self
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    public function getOrdering(): ?int
+    {
+        return $this->ordering;
+    }
+
+    public function setOrdering(int $ordering): self
+    {
+        $this->ordering = $ordering;
+
+        return $this;
+    }
+
+    public function getCdate(): ?\DateTimeInterface
+    {
+        return $this->cdate;
+    }
+
+    public function setCdate(\DateTimeInterface $cdate): self
+    {
+        $this->cdate = $cdate;
+
+        return $this;
+    }
+
+    public function getMdate(): ?\DateTimeInterface
+    {
+        return $this->mdate;
+    }
+
+    public function setMdate(\DateTimeInterface $mdate): self
+    {
+        $this->mdate = $mdate;
+
+        return $this;
+    }
+
+}
