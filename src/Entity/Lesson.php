d96f8f7bee2d3fbddd5b73711b26663a096a0200
Miguel Ángel <migueabellan@gmail.com>
2018-11-14 17:52:03 +0100

    Add Lesson entity

---
+++ src/Entity/Lesson.php
@@ -0,0 +1,318 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * Lesson
+ *
+ * @ORM\Table(name="lessons")
+ * @ORM\Entity(repositoryClass="App\Repository\LessonRepository")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class Lesson
+{
+    const ACCESS_USER       = 1;
+    const ACCESS_PREMIUM    = 2;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id_lesson", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var \Course
+     *
+     * @ORM\ManyToOne(targetEntity="Course")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_course", referencedColumnName="id_course", nullable=false)
+     * })
+     */
+    private $course;
+
+    /**
+     * @var \Teacher
+     *
+     * @ORM\ManyToOne(targetEntity="Teacher")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_teacher", referencedColumnName="id_teacher", nullable=false)
+     * })
+     */
+    private $teacher;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="alias", type="string", length=255)
+     */
+    private $alias;
+
+    /**
+     * @var text
+     *
+     * @ORM\Column(name="description", type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="video", type="string", nullable=true)
+     */
+    private $video;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(name="files", type="json_array", nullable=true)
+     */
+    private $files = array();
+	
+	/**
+     * @var integer
+     *
+     * @ORM\Column(name="score", type="integer")
+     */
+    private $score;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="active", type="integer")
+     */
+    private $active;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="access", type="integer")
+     */
+    private $access;
+
+    /**
+     * @var integer
+     *
+     * @Gedmo\SortablePosition
+     * @ORM\Column(name="position", type="integer")
+     */
+    private $position;
+
+    /**
+     * @var \DateTime
+     * 
+     * @ORM\Column(name="cdate", type="datetime")
+     */
+    private $cdate;
+
+    /**
+     * @var \DateTime
+     * 
+     * @ORM\Column(name="mdate", type="datetime")
+     */
+    private $mdate;
+
+
+
+    public function __construct()
+    {
+        $this->score    = 0;
+        $this->active   = false;
+        $this->access   = self::ACCESS_USER;
+        $this->cdate    = new \DateTime();
+        $this->mdate    = new \DateTime();
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function PrePersist()
+    {
+        $this->cdate    = new \DateTime();
+    }
+
+    /**
+     * @ORM\PreUpdate()
+     */
+    public function preUpdate()
+    {
+        $this->mdate    = new \DateTime();
+    }
+
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getAlias(): ?string
+    {
+        return $this->alias;
+    }
+
+    public function setAlias(string $alias): self
+    {
+        $this->alias = $alias;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getVideo(): ?string
+    {
+        return $this->video;
+    }
+
+    public function setVideo(?string $video): self
+    {
+        $this->video = $video;
+
+        return $this;
+    }
+
+    public function getFiles()
+    {
+        return $this->files;
+    }
+
+    public function setFiles($files): self
+    {
+        $this->files = $files;
+
+        return $this;
+    }
+
+    public function getScore(): ?int
+    {
+        return $this->score;
+    }
+
+    public function setScore(int $score): self
+    {
+        $this->score = $score;
+
+        return $this;
+    }
+
+    public function getActive(): ?int
+    {
+        return $this->active;
+    }
+
+    public function setActive(int $active): self
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    public function getAccess(): ?int
+    {
+        return $this->access;
+    }
+
+    public function setAccess(int $access): self
+    {
+        $this->access = $access;
+
+        return $this;
+    }
+
+    public function getPosition(): ?int
+    {
+        return $this->position;
+    }
+
+    public function setPosition(int $position): self
+    {
+        $this->position = $position;
+
+        return $this;
+    }
+
+    public function getCdate(): ?\DateTimeInterface
+    {
+        return $this->cdate;
+    }
+
+    public function setCdate(\DateTimeInterface $cdate): self
+    {
+        $this->cdate = $cdate;
+
+        return $this;
+    }
+
+    public function getMdate(): ?\DateTimeInterface
+    {
+        return $this->mdate;
+    }
+
+    public function setMdate(\DateTimeInterface $mdate): self
+    {
+        $this->mdate = $mdate;
+
+        return $this;
+    }
+
+    public function getCourse(): ?Course
+    {
+        return $this->course;
+    }
+
+    public function setCourse(?Course $course): self
+    {
+        $this->course = $course;
+
+        return $this;
+    }
+
+    public function getTeacher(): ?Teacher
+    {
+        return $this->teacher;
+    }
+
+    public function setTeacher(?Teacher $teacher): self
+    {
+        $this->teacher = $teacher;
+
+        return $this;
+    }
+
+}
