590109f474bd37e4319be39b4444b8466e646afa
Miguel Ángel <migueabellan@gmail.com>
2018-12-13 17:00:43 +0100

    Add encore webpack bootstrap

--- config/bundles.php
+++ config/bundles.php
@@ -22,4 +22,5 @@ return [
     Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
     Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['all' => true],
     Sonata\TranslationBundle\SonataTranslationBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
