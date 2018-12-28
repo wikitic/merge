961749bc014e504827c214f2f6d006e5ac5b4b9d
Miguel Ángel <migueabellan@gmail.com>
2018-12-28 22:03:39 +0100

    Add partners controller api

--- src/Controller/api/v1/PartnerController.php
+++ src/Controller/api/v1/PartnerController.php
@@ -2,26 +2,23 @@
 
 namespace App\Controller\api\v1;
 
-use App\Entity\Partner;
 use App\Service\PartnerService;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\Serializer\SerializerInterface;
 
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use FOS\RestBundle\View\View;
-
-
+/**
+ * Partner controller.
+ *
+ * @Route("/api/v1")
+ */
 class PartnerController extends AbstractController
 {
-
     /** @var SerializerInterface */
     private $serializer;
 
@@ -41,32 +38,14 @@ class PartnerController extends AbstractController
 
 
      /**
-     * @Rest\Get("/api/v1/partners", name="getAllPosts")
+     * @Rest\Get("/partners", name="getPartners")
      * @return JsonResponse
      */
-    public function getAllActions(): JsonResponse
+    public function getPartners(): JsonResponse
     {
         $partnerService = $this->partnerService->getAll();
         $data = $this->serializer->serialize($partnerService, 'json');
 
-        return new JsonResponse($data, 200, [], true);
+        return new JsonResponse($data, Response::HTTP_OK, [], true);
     }
-
-
-
-
-
-    /**
-     * @Route("/api/v1/autors", methods={"GET"})
-     * @param Request $request
-     */
-    public function getPartners(Request $request): View
-    {        
-        $em = $this->getDoctrine()->getManager();
-        $partners = $em->getRepository(Partner::class)->findBy([]);
-                
-        return View::create($partners, Response::HTTP_OK);
-    }
-
-
 }
