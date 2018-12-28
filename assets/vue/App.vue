3ed5293a00ae866ff780b5bb6014257e2760ab1c
Miguel Ángel <migueabellan@gmail.com>
2018-12-28 20:18:35 +0100

    Add vue vue-route

---
+++ assets/vue/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <router-link class="navbar-brand" to="/home">App</router-link>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <router-link class="nav-item" tag="li" to="/home" active-class="active">
+                        <a class="nav-link">Home</a>
+                    </router-link>
+                </ul>
+            </div>
+        </nav>
+
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+    }
+</script>
--- webpack.config.js
+++ webpack.config.js
@@ -17,14 +17,14 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
-    .addEntry('css/app', './assets/css/app.scss')
-    .addEntry('js/app', './assets/js/app.js')
-    //.addEntry('page1', './assets/js/page1.js')
-    //.addEntry('page2', './assets/js/page2.js')
+    .addEntry('app', './assets/vue/index.js')
+    //.addEntry('css/app', './assets/css/app.scss')
+    //.addEntry('js/app', './assets/js/app.js')
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
-    .enableSingleRuntimeChunk()
+    //.enableSingleRuntimeChunk()
+    .disableSingleRuntimeChunk()
 
     /*
      * FEATURE CONFIG
@@ -38,6 +38,7 @@ Encore
     .enableSourceMaps(!Encore.isProduction())
     // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
+    .enableVueLoader()
 
     // enables Sass/SCSS support
     .enableSassLoader()
