adadcbd99407509390b33a3f41dbdb0fa388725b
Miguel Ángel <migueabellan@gmail.com>
2018-12-30 12:43:45 +0100

    Refactor theme admin

--- assets/js/App.vue
+++ assets/js/App.vue
@@ -1,9 +1,51 @@
 <template>
     <v-app>
 
-        <navigation v-if="isAuthenticated"></navigation>
+        <v-navigation-drawer v-if="isAuthenticated" v-model="drawer" app fixed>
+            <v-toolbar flat>
+                <v-avatar size="120px">
+                    <img src="../images/logo.svg" />
+                </v-avatar>
+            </v-toolbar>
+            <v-divider></v-divider>
+            <v-list>
+                <v-list-tile v-for="item in navigation" :key="item.title" :to="item.link">
+                    <v-list-tile-action>
+                        <v-icon>
+                            {{ item.icon }}
+                        </v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title>
+                            {{ item.title }}
+                        </v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
 
-        <toolbar v-if="isAuthenticated"></toolbar>
+        <v-toolbar color="primary" v-if="isAuthenticated" dark fixed app>
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title></v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-menu offset-y origin="center center" :nudge-bottom="10" transition="scale-transition">
+                <v-btn icon large flat slot="activator">
+                    <v-avatar size="30px">
+                        <img src="../images/icono.jpg" />
+                    </v-avatar>
+                </v-btn>
+                <v-list class="pa-0">
+                    <v-list-tile v-for="item in toolbar" :key="item.title" :to="item.href">
+                        <v-list-tile-action v-if="item.icon">
+                            <v-icon>{{ item.icon }}</v-icon>
+                        </v-list-tile-action>
+                        <v-list-tile-content>
+                            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                </v-list>
+            </v-menu>
+        </v-toolbar>
 
         <v-content>
             <router-view></router-view>
@@ -20,14 +62,21 @@
 
 <script>
     import axios from 'axios';
-    import Navigation from './components/Navigation';
-    import Toolbar from './components/Toolbar';
 
     export default {
         name: 'app',
-        components: {
-            Navigation,
-            Toolbar,
+        data () {
+            return {
+                drawer: null,
+                navigation: [
+                    { title: 'Dashboard', icon: 'home', link: '/dashboard' },
+                    { title: 'Socios', icon: 'account_circle', link: '/partners' },
+                ],
+                toolbar: [
+                    { title: 'Profile', icon: 'account_circle', href: '/profile' },
+                    { title: 'Logout', icon: 'fullscreen_exit', href: '/logout' }
+                ]
+            }
         },
         created () {
             let isAuthenticated = JSON.parse(this.$parent.$el.attributes['data-is-authenticated'].value),
