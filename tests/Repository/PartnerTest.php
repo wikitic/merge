4374c8ce5b596e06eab33572d74135d168e90c79
Miguel Ángel <migueabellan@gmail.com>
2019-01-04 19:51:38 +0100

    Add partner repository tests

--- tests/Repository/PartnerTest.php
+++ tests/Repository/PartnerTest.php
@@ -9,23 +9,15 @@ use Symfony\Component\HttpFoundation\Response;
 
 class PartnerTest extends WebTestCase
 {
-    private $client;
-    private $em;
     private $er;
 
     protected function setUp()
     {
-        $kernel = self::bootKernel();
-        $this->client = $this->createClient(['environment' => 'test']);
-        $this->client->disableReboot();
-        $this->em = $this->client->getContainer()->get('doctrine.orm.entity_manager');
-        $this->em->beginTransaction();
-        $this->er = $this->em->getRepository(Partner::class);
-    }
-
-    protected function tearDown()
-    {
-        $this->em->rollback();
+        $client = $this->createClient(['environment' => 'test']);
+        $client->disableReboot();
+        $em = $client->getContainer()->get('doctrine.orm.entity_manager');
+        $em->beginTransaction();
+        $this->er = $em->getRepository(Partner::class);
     }
 
     public function testGetUniqueCode()
@@ -33,5 +25,35 @@ class PartnerTest extends WebTestCase
         $code = $this->er->getUniqueCode();
 
         $this->assertEquals(6, strlen($code));
+    }
+    
+    /**
+     * @dataProvider provideRequestValidate
+     */
+    public function testRequestValidate($method = null, $data = null, $assert = null)
+    {
+        $bool = $this->er->requestValidate($data, $method);
+        $this->assertEquals($assert, $bool);
+    }
+    public function provideRequestValidate()
+    {
+        yield ['POST', [], false];
+        yield ['POST', [                'surname'=>'surname', 'email'=>'GOOD@EMAIL.com'], false];
+        yield ['POST', ['name'=>'name'                      , 'email'=>'GOOD@EMAIL.com'], false];
+        yield ['POST', ['name'=>'name', 'surname'=>'surname'                           ], false];
+        yield ['POST', ['name'=>'name', 'surname'=>'surname', 'email'=>'BAD_EMAIL'     ], false];
+        yield ['POST', ['name'=>''    , 'surname'=>'surname', 'email'=>'GOOD@EMAIL.com'], false];
+        yield ['POST', ['name'=>'name', 'surname'=>''       , 'email'=>'GOOD@EMAIL.com'], false];
+        yield ['POST', ['name'=>'name', 'surname'=>'surname', 'email'=>''              ], false];
+        yield ['POST', ['name'=>'name', 'surname'=>'surname', 'email'=>'GOOD@EMAIL.com'], true];
+
+        yield ['PATCH', [], true];
+        yield ['PATCH', ['name'=>''], false];
+        yield ['PATCH', ['name'=>'name'], true];
+        yield ['PATCH', ['surname'=>''], false];
+        yield ['PATCH', ['surname'=>'surname'], true];
+        yield ['PATCH', ['email'=>''], false];
+        yield ['PATCH', ['email'=>'BAD_EMAIL'], false];
+        yield ['PATCH', ['email'=>'GOOD@EMAIL.com'], true];
     }
 }
