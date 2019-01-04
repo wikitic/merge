38a942793f2901f391ed97669a5e5d1440964c4b
Miguel Ángel <migueabellan@gmail.com>
2019-01-04 14:41:06 +0100

    Refactor front delete

--- assets/js/components/Partners/PartnerDelete.vue
+++ assets/js/components/Partners/PartnerDelete.vue
@@ -3,34 +3,30 @@
         <v-btn v-if="hasSubscriptions" slot="activator" flat icon color="grey">
             <v-icon>delete</v-icon>
         </v-btn>
+
         <v-card>
-            <v-card-title>
-                <span class="headline">Borrar socio</span>
-            </v-card-title>
+            <v-toolbar color="orange darken-2" dark>
+                <v-toolbar-title>Borrar Socio</v-toolbar-title>
+            </v-toolbar>
             <v-card-text>
-                <v-alert v-if="!isDisabled" :value="true" type="warning">
-                    ¿Seguro que quieres borrar a <b>{{ partner.fullname }}</b>?
-                </v-alert>
-                <v-alert v-if="hasError" :value="true" color="error" icon="warning">
-                    {{ error.response.status }} {{ error.response.statusText }}
-                </v-alert>
-                <v-container grid-list-md>
-                    <v-layout wrap>
-                        <v-flex xs12>
-                            <v-text-field label="Código" v-model="partner.code" disabled></v-text-field>
-                        </v-flex>
-                        <v-flex xs12>
-                            <v-text-field label="Confirma código*" v-model="confirm.code" required maxlength="6" counter="6"></v-text-field>
-                        </v-flex>
-                    </v-layout>
-                </v-container>
-                <small>* Campos obligatorios</small>
+                <form @submit.prevent="submit">
+                    <v-container grid-list-md>
+                        <v-layout wrap>
+                            <v-flex xs12>
+                                <v-text-field label="Código" v-model="partner.code" disabled></v-text-field>
+                            </v-flex>
+                            <v-flex xs12>
+                                <v-text-field label="Confirma código *" v-model="confirm.code" required maxlength="6" counter="6"></v-text-field>
+                            </v-flex>
+                        </v-layout>
+                    </v-container>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn flat @click="dialog = false">Cerrar</v-btn>
+                        <v-btn type="submit" :disabled="isDisabled" color="primary">Borrar</v-btn>
+                    </v-card-actions>
+                </form>
             </v-card-text>
-            <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="darken-1" flat @click="dialog = false">Cerrar</v-btn>
-                <v-btn color="orange" :disabled="isDisabled" class="primary" @click="deletePartner()">Borrar</v-btn>
-            </v-card-actions>
         </v-card>
     </v-dialog>
 </template>
@@ -40,31 +36,24 @@
     export default {
         name: 'partner-edit',
         props: ['partner'],
-        data () {
-            return {
-                dialog: false,
-                confirm: {
-                    code: null
-                }
+        data: () => ({
+            dialog: false,
+            confirm: {
+                code: ''
             }
-        },
+        }),
         computed: {
             hasSubscriptions () {
                 return this.partner.subscriptions.length === 0
             },
             isDisabled () {
                 return this.partner.code !== this.confirm.code
-            },
-            hasError () {
-                return this.$store.getters['partner/hasError'];
-            },
-            error () {
-                return this.$store.getters['partner/error'];
-            },
+            }
         },
         methods: {
-            deletePartner () {
+            submit () {
                 this.$store.dispatch('partner/deletePartners', this.partner)
+                    .then(() => { this.dialog = false })
             }
         }
     }
