8a49690241a4d648b59c5a5f87837715fcfabd86
Miguel Ángel <migueabellan@gmail.com>
2019-01-07 11:07:48 +0100

    Add subscriptions

--- src/Controller/api/v1/SubscriptionController.php
+++ src/Controller/api/v1/SubscriptionController.php
@@ -13,6 +13,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
 
 /**
  * Subscription controller
@@ -38,16 +42,41 @@ final class SubscriptionController extends AbstractController
     {
         $this->em = $em;
         $this->er = $em->getRepository(Subscription::class);
+
+        // Evitar "A circular reference has been detected when serializing the object of class"
+        $normalizer = new ObjectNormalizer();
+        $normalizer->setIgnoredAttributes(['subscriptions']);
+        $serializer = new Serializer([$normalizer], [new JsonEncoder()]);
+        // Evitar ...
+        
         $this->serializer = $serializer;
     }
 
 
+
+    /**
+     * @Route("/subscriptions", methods={"GET"})
+     *
+     * @return JsonResponse
+     */
+    public function getSubscriptions(): JsonResponse
+    {
+        $subscriptions = $this->er->findBy([]);
+
+        return new JsonResponse(
+            $this->serializer->serialize($subscriptions, 'json'),
+            Response::HTTP_OK,
+            [],
+            true
+        );
+    }
+    
     /**
      * @Rest\Get("/partners/{id_partner}/subscriptions", name="getSubscriptions")
      * @param string $id_partner
      * @return JsonResponse
      */
-    public function getSubscriptions(string $id_partner = null): JsonResponse
+    public function getPartnersSubscriptions(string $id_partner = null): JsonResponse
     {
         $subscriptions = $this->er->findBy(['partner' => $id_partner], ['inDate' => 'DESC']);
 
