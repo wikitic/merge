0a4fe3caeee28e9fa867f70c49873fd66a69683c
Miguel Ángel <migueabellan@gmail.com>
2019-01-09 13:52:28 +0100

    Add list sobscriptions

--- assets/js/views/Partners/Subscriptions/List.vue
+++ assets/js/views/Partners/Subscriptions/List.vue
@@ -1,29 +1,18 @@
 <template>
     <v-card color="grey lighten-4" class="white--text">
-        <v-toolbar flat color="white">
-            <v-spacer></v-spacer>
-            <subscriptions-new :partner="partner"></subscriptions-new>
-        </v-toolbar>
         <v-card-text>
             <v-data-table :headers="headers" :items="partner.subscriptions" item-key="id" :pagination.sync="pagination" hide-actions>
                 <template slot="items" slot-scope="props">
                     <tr>
+                        <td>{{ props.item.id }}</td>
                         <td>
-                            <v-chip :color="getColorByActive(isActive(props.item.inDate, props.item.outDate))" outline>
-                                {{ toDate(props.item.inDate) }}
-                            </v-chip>
+                            <v-icon v-if="isSubscriber(props.item)" x-large color="green">done</v-icon>
+                            <v-icon v-if="!isSubscriber(props.item)" x-large color="red">clear</v-icon>
                         </td>
-                        <td>
-                            <v-chip :color="getColorByActive(isActive(props.item.inDate, props.item.outDate))" outline>
-                                {{ toDate(props.item.outDate) }}
-                            </v-chip>
-                        </td>
+                        <td>{{ toDate(props.item.inDate) }}</td>
+                        <td>{{ toDate(props.item.outDate) }}</td>
                         <td>{{ props.item.info }}</td>
                         <td>{{ props.item.price }}</td>
-                        <td>
-                            <subscriptions-edit :subscription="props.item"></subscriptions-edit>
-                            <subscriptions-delete :partner="partner" :subscription="props.item"></subscriptions-delete>
-                        </td>
                     </tr>
                 </template>
             </v-data-table>
@@ -32,31 +21,24 @@
 </template>
 
 <script>
-    import SubscriptionsNew from './New'
-    import SubscriptionsEdit from './Edit'
-    import SubscriptionsDelete from './Delete'
-
     export default {
         name: 'partners-subscriptions',
-        props: ['partner'],
-        components: {
-            SubscriptionsNew,
-            SubscriptionsEdit,
-            SubscriptionsDelete
+        props: {
+            partner: {
+                type: Object,
+                default: null
+            }
         },
         data () {
             return {
                 headers: [
+                    { text: '#', value: 'id', sortable: false, width: 10 },
+                    { text: 'Estado', value: '', sortable: false, width: 50 },
                     { text: 'Inicio', value: 'inDate', sortable: false, width:100 },
                     { text: 'Fin', value: 'outDate', sortable: false, width:100 },
                     { text: 'Info', value: 'info', sortable: false },
-                    { text: 'Price', value: 'price', sortable: false, width:100 },
-                    { text: '', sortable: false, width: 200 }
+                    { text: 'Price', value: 'price', sortable: false, width:100 }
                 ],
-                colors: {
-                    true: 'green',
-                    false: 'red'
-                },
                 pagination: {
                     rowsPerPage: -1
                 }
@@ -67,14 +49,15 @@
                 let date = new Date(datetime)
                 return date.toLocaleDateString("es-ES") //eu-ES
             },
-            isActive(inDate, outDate) {
+            isSubscriber (subscription) {
                 let now = new Date()
-                inDate = new Date(inDate)
-                outDate = new Date(outDate)
-                return now > inDate && now < outDate
-            },
-            getColorByActive (status) {
-                return this.colors[status];
+                let inDate = new Date(subscription.inDate)
+                let outDate = new Date(subscription.outDate)
+
+                if(now > inDate && now < outDate)
+                    return 1
+                else
+                    return 0
             }
         }
     }
