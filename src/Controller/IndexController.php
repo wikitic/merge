d4b2225b70c92d8df7bf1d1806bd6e7b532bd91d
Miguel Ángel <migueabellan@gmail.com>
2019-01-28 16:45:52 +0100

    Add metas backend

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Language;
+use App\Entity\Module;
+use App\Entity\Lesson;
+
 use App\BundlePartner\Entity\Partner;
 use App\BundlePartner\Repository\PartnerRepository;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -31,8 +36,21 @@ class IndexController extends AbstractController
      *
      * @return Response
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+
+        $metas= '';
+        //dump($request->getPathInfo()); die;
+        /*
+        $em = $this->getDoctrine()->getManager();
+        $language = $em->getRepository(Language::class)->findOneBy(['alias'=>'blockly']);
+        $module = $em->getRepository(Module::class)->findOneBy(['language'=>$language, 'alias'=>'hora-del-codigo']);
+        $lesson = $em->getRepository(Lesson::class)->findOneBy(['module'=>$module, 'alias'=>'mi-primer-programa']);
+        $metas = $lesson;
+        */
+        //dump($lesson); die;
+
+        
         $partner = $this->getUser();
 
         $partner = $this->pr->serialize($partner);
@@ -40,6 +58,7 @@ class IndexController extends AbstractController
         return $this->render(
             'base.html.twig',
             [
+                'metas' => $metas,
                 'partner' => $partner
             ]
         );
