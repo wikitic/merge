a7a873b9bef6eed0349bb09b759f8cfc8a2d56d7
Miguel Ángel <migueabellan@gmail.com>
2019-02-25 20:54:02 +0100

    Add jwt nelmio

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,7 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
