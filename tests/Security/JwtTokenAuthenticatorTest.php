8466a3a077c7a79896d8acb0bea22a7e5f971351
Miguel Ángel <migueabellan@gmail.com>
2019-02-26 10:41:14 +0100

    Add jwt test

--- tests/Security/JwtTokenAuthenticatorTest.php
+++ tests/Security/JwtTokenAuthenticatorTest.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Tests\Controller\v1;
+namespace App\Tests\Security;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
-class CategoryTest extends WebTestCase
+class JwtTokenAuthenticatorTest extends WebTestCase
 {
     private $client = null;
 
@@ -28,9 +28,10 @@ class CategoryTest extends WebTestCase
             ->get('lexik_jwt_authentication.encoder')
             ->encode($user);
 
-            
+
         $client = $this->client;
         $client->setServerParameter('HTTP_Authorization', sprintf('Bearer %s', $token));
+
         return $client;
     }
 
@@ -39,10 +40,10 @@ class CategoryTest extends WebTestCase
     /**
      * @dataProvider provide_GET_url
      */
-    public function test_GET_url($url = null, $http_code = null, $data = null)
+    public function test_GET_url($method = null, $url = null, $http_code = null, $data = null)
     {
         $client = $this->client;
-        $client->request('GET', $url, [], [], ['CONTENT_TYPE' => 'application/json'], $data);
+        $client->request($method, $url, [], [], ['CONTENT_TYPE' => 'application/json'], $data);
 
         $response = $client->getResponse();
         $content = $response->getContent();
@@ -52,50 +53,39 @@ class CategoryTest extends WebTestCase
     public function provide_GET_url()
     {
         $HTTP_OK = Response::HTTP_OK;                                 // 200
-        $HTTP_FOUND = Response::HTTP_FOUND;                           // 302
-        $HTTP_METHOD_NOT_ALLOWED = Response::HTTP_METHOD_NOT_ALLOWED; // 405
-        $HTTP_BAD_REQUEST = Response::HTTP_BAD_REQUEST;               // 400
         $HTTP_UNAUTHORIZED = Response::HTTP_UNAUTHORIZED;             // 401
 
-        yield ['/v1/categories',  $HTTP_OK];
-        
-        yield ['/v1/categories/categoria-1', $HTTP_OK];
-        yield ['/v1/categories/categoria-2', $HTTP_FOUND]; // disabled
-        yield ['/v1/categories/BAD-ALIAS',   $HTTP_FOUND];
+        yield ['GET',   '/v1/categories',  $HTTP_OK];
+        yield ['POST',  '/v1/categories',  $HTTP_UNAUTHORIZED, json_encode([])];
     }
 
 
-
     /**
-     * @dataProvider provide_POST_url
+     * @dataProvider provide_GET_url_aut
      */
-    public function test_POST_url($url = null, $http_code = null, $data = null)
+    public function test_GET_url_aut($method = null, $url = null, $http_code = null, $data = null)
     {
-        //$client = $this->client;
         $user = [
             'username' => 'admin',
             'aud' => 'admin'
         ];
-        $client = $this->createAuthenticatedClient($user);
 
-        $client->request('POST', $url, [], [], ['CONTENT_TYPE' => 'application/json'], $data);
+        $client = $this->createAuthenticatedClient($user);
+        $client->request($method, $url, [], [], ['CONTENT_TYPE' => 'application/json'], $data);
 
         $response = $client->getResponse();
         $content = $response->getContent();
 
         $this->assertEquals($http_code, $response->getStatusCode());
     }
-    public function provide_POST_url()
+    public function provide_GET_url_aut()
     {
         $HTTP_OK = Response::HTTP_OK;                                 // 200
-        $HTTP_FOUND = Response::HTTP_FOUND;                           // 302
-        $HTTP_METHOD_NOT_ALLOWED = Response::HTTP_METHOD_NOT_ALLOWED; // 405
         $HTTP_BAD_REQUEST = Response::HTTP_BAD_REQUEST;               // 400
         $HTTP_UNAUTHORIZED = Response::HTTP_UNAUTHORIZED;             // 401
 
         $category = [
             'title' => 'title',
-            'alias' => 'categoria-1', // Exist
             'alias' => 'alias',
             'description' => 'description',
             'metatitle' => 'metatitle',
@@ -105,13 +95,9 @@ class CategoryTest extends WebTestCase
             'active' => '0'
         ];
 
-        foreach ($category as $k => $v) {
-            $aux = $category;
-            unset($aux[$k]);
-            yield ['/v1/categories',  $HTTP_BAD_REQUEST, json_encode($aux)];
-        }
-
-        yield ['/v1/categories',  $HTTP_BAD_REQUEST, json_encode([])];
-        yield ['/v1/categories',  $HTTP_OK, json_encode($category)];
+        yield ['GET',   '/v1/categories',  $HTTP_OK];
+        yield ['POST',  '/v1/categories',  $HTTP_BAD_REQUEST, json_encode([])];
+        yield ['POST',  '/v1/categories',  $HTTP_OK, json_encode($category)];
     }
+
 }
