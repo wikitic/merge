a27affc220c4f127dbeebce4989fcb86a36505f0
Miguel Ángel <migueabellan@gmail.com>
2019-04-28 13:00:39 +0200

    Fix test forkme

--- test/components/Layout/Forkme.spec.js
+++ test/components/Layout/Forkme.spec.js
@@ -8,6 +8,7 @@ Vue.use(Bootstrap)
 describe('Forkme', () => {
   it('renders', () => {
     const wrapper = mount(Forkme)
-    expect(wrapper.html()).toContain('href=\"https://github.com/wikitic/wikitic.github.io\"')
+    const href = 'https://github.com/wikitic/wikitic.github.io'
+    expect(wrapper.html()).toContain(href)
   })
 })
