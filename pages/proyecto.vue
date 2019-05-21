3fb150f921c96b67d79f0903494800535a9be48b
Miguel Ángel <migueabellan@gmail.com>
2019-05-21 18:46:05 +0200

    Update project

--- pages/proyecto.vue
+++ pages/proyecto.vue
@@ -12,19 +12,19 @@
       >
         <Metas />
         <main>
-          <h1 class="mb-3 display-1 font-weight-bold">
+          <h1 class="mb-3 display-1">
             ¿Qué es WikiTIC?
           </h1>
           <p class="headline font-weight-light">
             WikiTIC es un proyecto educativo de código abierto que surge de la necesidad de fomentar el uso de las TIC (<i>Tecnologías de la Información y la Comunicación</i>) en los centros educativos, principalmente en los niveles de la ESO y Bachillerato.
           </p>
-          <h1 class="mt-5 mb-3 display-1 font-weight-bold">
+          <h1 class="mt-5 mb-3 display-1">
             ¿Quién está detrás de WikiTIC?
           </h1>
           <p class="headline font-weight-light">
             WikiTIC es otro proyecto educativo creado y gestionado por la <a href="https://www.programoergosum.com">Asociación Programo Ergo Sum</a>.
           </p>
-          <h1 class="mt-5 mb-3 display-1 font-weight-bold">
+          <h1 class="mt-5 mb-3 display-1">
             ¿Qué licencia tiene WikiTIC?
           </h1>
           <p class="headline font-weight-light">
