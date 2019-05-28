3a6902b7c9562bb629d147582f670b9cfff459b0
Miguel Ángel <migueabellan@gmail.com>
2019-05-28 11:58:31 +0200

    Update proyecto

--- pages/proyecto.vue
+++ pages/proyecto.vue
@@ -12,24 +12,21 @@
       >
         <Metas />
         <main>
-          <h1 class="mb-3 display-1">
-            ¿Qué es WikiTIC?
-          </h1>
-          <p class="headline font-weight-light">
-            WikiTIC es un proyecto educativo de código abierto que surge de la necesidad de fomentar el uso de las TIC (<i>Tecnologías de la Información y la Comunicación</i>) en los centros educativos, principalmente en los niveles de la ESO y Bachillerato.
-          </p>
-          <h1 class="mt-5 mb-3 display-1">
-            ¿Quién está detrás de WikiTIC?
-          </h1>
-          <p class="headline font-weight-light">
-            WikiTIC es otro proyecto educativo creado y gestionado por la <a href="https://www.programoergosum.com">Asociación Programo Ergo Sum</a>.
-          </p>
-          <h1 class="mt-5 mb-3 display-1">
-            ¿Qué licencia tiene WikiTIC?
-          </h1>
-          <p class="headline font-weight-light">
-            Todo el contenido incluída la propia página web están alojados en repositorios públicos de GitHub. La página web tiene una licencia MIT y todo el contenido CC-BY-SA 4.0.
-          </p>
+          <div class="mb-5">
+            <h1>Proyecto Educativo WikiTIC</h1>
+            <p class="headline font-weight-light">
+              WikiTIC es un proyecto educativo <strong>100% código abierto</strong> creado y gestionado por la <a href="https://www.programoergosum.com">Asociación Programo Ergo Sum</a>, para fomentar el uso de las <strong>TIC</strong> (<i>Tecnologías de la Información y la Comunicación</i>) en los centros educativos en los niveles de la <strong>ESO</strong> y <strong>Bachillerato</strong>.
+            </p>
+          </div>
+          <div class="mb-5 text-xs-center">
+            <h1>Patrocinadores</h1>
+            <img width="250px" src="~/static/logo-programoergosum.png" title="Asociación Programo Ergo Sum">
+          </div>
+          <div class="mb-5 text-xs-center">
+            <h1>Colaboradores</h1>
+            <img width="250px" src="~/static/logo-aytomurcia.png" title="Ayuntamiento de Murcia">
+            <img width="250px" src="~/static/logo-cprmurcia.png" title="Centro de Profesorado y Recursos">
+          </div>
         </main>
       </v-flex>
     </v-layout>
@@ -47,4 +44,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+main {
+  font-size: 18px;
+  h1 {
+    margin-bottom: 20px;
+    font-size: 1.5em;
+    font-weight: 600;
+    line-height: 50px;
+  }
+  img {
+    margin: 10px 20px 40px;
+  }
+}
 </style>
