c9c650f79c524fef38600cf6383fe1bba5ca364d
Miguel Ángel <migueabellan@gmail.com>
2019-09-20 11:09:23 +0200

    Remove

--- es/src/3.py
+++
@@ -1,5 +0,0 @@
-from microbit import *
-
-for i in range(9,-1,-1):
-  display.show(i)
-  sleep(500)
--- es/src/4.py
+++
@@ -1,6 +0,0 @@
-from microbit import *
-import random
-
-while True:
-  display.show(random.randint(0, 9))
-  sleep(500)
